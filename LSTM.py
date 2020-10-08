6fd513e039f19f36940516126f99ad7c771a2ec1
olokojoh <54641961+olokojoh@users.noreply.github.com>
2020-10-08 17:01:07 -0400

    merge preprocessing

---
+++ LSTM.py
@@ -0,0 +1,18 @@
+import math
+import matplotlib.pyplot as plt
+import keras
+import pandas as pd
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.layers import Dropout
+from keras.layers import *
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import train_test_split
+from keras.callbacks import EarlyStopping
+df=pd.read_csv("TSLA.csv")
+print(‘Number of rows and columns:’, df.shape)
+df.head(5)
--- Time Series model.py
+++ Time Series model.py
@@ -1,81 +1,15 @@
+#%%
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import statsmodels.api as sm
 from scipy import signal
+from Preprocessing import get_auto_corr
 
 #%%
 #import data
-import random
-random.seed(42)
-pd.set_option('display.width', 400)
-pd.set_option('display.max_columns', 15)
-df=pd.read_csv('LBMA-GOLD.csv')
-df.head(5)
 
-#%%
-##EDA and preprocessing
-#%%
-#check missing value
-def nan_checker(df):
-    df_nan = pd.DataFrame([[var, df[var].isna().sum() / df.shape[0], df[var].dtype]
-                           for var in df.columns if df[var].isna().sum() > 0],
-                          columns=['var', 'proportion', 'dtype'])
-    df_nan = df_nan.sort_values(by='proportion', ascending=False)
-    return df_nan
-df_nan = nan_checker(df)
-df_nan.reset_index(drop=True)
-
-#%%
-#sort in date in ascending order
-df['Date'] = pd.to_datetime(df.Date,format='%Y-%m-%d')
-df.sort_values(by='Date', inplace=True, ascending=True)
-df.head(5)
-
-#%%
-#change the index to be the date
-df.index = df['Date']
-df.drop('Date',axis = 1, inplace = True)
-df.head(5)
-
-#%%
-#pick usd(am) as target value and create a new dataframe
-df.columns.values[0] = 'USD'
-df.head(5)
-
-#%%
-gold = df.iloc[: , [0]].copy()
-gold.head(5)
-#%%
-#see how many missing values
-gold.isnull().sum()
-#1 missing value
-#%%
-gold.info()
-#%%
-gold.dropna(inplace=True)
-gold.info()
-#%%
-df_nan = nan_checker(gold)
-df_nan.reset_index(drop=True)
-
-#%%
-gold.head()
-
-#%%
-def get_auto_corr(timeSeries,k):
-    l = len(timeSeries)
-    timeSeries1 = timeSeries[0:l-k]
-    timeSeries2 = timeSeries[k:]
-    timeSeries_mean = np.mean(timeSeries)
-    timeSeries_var = np.array([i**2 for i in timeSeries-timeSeries_mean]).sum()
-    auto_corr = 0
-    for i in range(l-k):
-        temp = (timeSeries1[i]-timeSeries_mean)*(timeSeries2[i]-timeSeries_mean)/timeSeries_var
-        auto_corr = auto_corr + temp
-    return auto_corr
-
-#%%
+gold = pd.read_csv('./data/gold.csv')
 #plot the target value
 plt.plot(gold['USD'])
 plt.xlabel('date')
